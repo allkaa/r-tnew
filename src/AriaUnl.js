1ab761b522e402af967f27f5307b901552fa3b19
AKaarna <AKaarna@AccountGt>
2020-07-20 15:16:46 +0300

    AriaUnl_001

--- src/AriaUnl.js
+++ src/AriaUnl.js
@@ -1,3 +1,4 @@
+// AriaUnl_001
 import React, { useState, useEffect } from 'react';
 
 // React function component.
@@ -8,18 +9,10 @@ function AriaUnl(props) {
   // State Hook samples:
   // Declare a new state variable, which we'll call "count" using destructuring assignment syntax
   // Two constants will be created count as number and setCount(args) as function.
-  const [count, setCount] = useState(0); // initial count state set as zero.
-  console.log('count:');
-  console.log(count);
-  console.log('typeof(count):');
-  console.log(typeof(count));
-  console.log('setCount:');
-  console.log(setCount);
-  console.log('useState:');
-  console.log(useState);
-  const [age, setAge] = useState(props.age);
-  const [fruit, setFruit] = useState('banana');
-  const [todos, setTodos] = useState(['text', ' ', 'Learn Hooks']);
+  //const [count, setCount] = useState(0); // initial count state set as zero.
+  //const [age, setAge] = useState(props.age);
+  const [searchTxt, setsearchTxt] = useState('');
+  //const [todos, setTodos] = useState(['text', ' ', 'Learn Hooks']);
   //const [todos, setTodos] = useState([{ key_learn: 'Learn Hooks' }]); // does not work - React child can not be object with key(s).
   //
   // Effect Hook samples:
@@ -33,30 +26,16 @@ function AriaUnl(props) {
   // First Effect Hook:
   //
   useEffect(() => {
-    if (count > 0) {
-      // Update the document title using the browser API:
-      document.title = `You clicked ${count} times at ${props.dattime}`;
-    }
+    document.title = `ARIA UNL`;
   });
-  // next Effect Hook:
-  // If you want to run an effect and clean it up only once (on mount and unmount), you can pass an empty array ([])
-  // as a second argument. 
-  useEffect(() => {window.alert(`count is ${count} at ${props.dattime}`)},[]);
-
   //
   // NB! Only one child can be returned!
   return (
     <div> 
-      <p>You clicked {count} times asshole!</p>
-      <p>You age is {age}</p>
-      <p>You fruit is {fruit}</p>
-      <p>You todos is {todos}</p>
+      <p>Search text is {searchTxt}</p>
       <button onClick={
         () => {
-        setCount(count + 1);
-        setAge(age - 1);
-        setFruit(fruit + '1');
-        setTodos([todos[0] + 0, ' - ',  todos[2]+2 ]);
+          setsearchTxt(searchTxt + '?');
         }
       }>
         Click me
