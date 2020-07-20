2a1946460ec999fa63cddb0014cb566e39aece63
AKaarna <AKaarna@AccountGt>
2020-07-20 15:01:53 +0300

    AriaUnl_001

--- src/AriaUnl.js
+++ src/AriaUnl.js
@@ -1,12 +1,9 @@
 import React, { useState, useEffect } from 'react';
 
 // React function component.
-function Hooks_Example(props) {
+function AriaUnl(props) {
   console.log('props:');
   console.log(props);
-  // Destructing assignment syntax sample:
-  const [a, b] = f(); // using destructuring assignment syntax to parse an array returned from a function.
-  console.log('a=',a,'b=',b);
   //
   // State Hook samples:
   // Declare a new state variable, which we'll call "count" using destructuring assignment syntax
@@ -68,35 +65,4 @@ function Hooks_Example(props) {
   );
 }
 
-export default Hooks_Example;
-
-function f() {
-  return [1, 'one'];
-}
-
-// Sample using class.
-// In React class components, the render method itself shouldn’t cause side effects. It would be too early - 
-// we typically want to perform our effects after React has updated the DOM in componentDidMount and componentDidUpdate.
-class Example extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { count: 0 };
-  }
-  componentDidMount() {
-    document.title = `You clicked ${this.state.count} times`;
-  }
-  componentDidUpdate() {
-    document.title = `You clicked ${this.state.count} times`;
-  }
-
-  render() {
-    return (
-      <div>
-        <p>You clicked {this.state.count} times</p>
-        <button onClick={() => this.setState({ count: this.state.count + 1 })}>
-          Click me
-        </button>
-      </div>
-    );
-  }
-}
+export default AriaUnl;
