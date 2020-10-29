75a3c5c4212277e76747c5bb508c23c1674d6d8e
AKaarna <AKaarna@AccountGt>
2020-10-29 13:36:54 +0200

    nodeServerUNL reload+cmd 1013

--- nodeServerUNL.js
+++ nodeServerUNL.js
@@ -1,4 +1,4 @@
-// nodeServerUNL reload+cmd 1012
+// nodeServerUNL reload+cmd 1013
 'use strict'; // is unnecessary inside of modules.
 // Using special formName  /formAKchk?q=123-12345678-1234567 /formAKval?q=123-12345678-1234567 or /formAKpay?q=xxx
 //file:///home/akaarna/react-tutorial/build/index.html
